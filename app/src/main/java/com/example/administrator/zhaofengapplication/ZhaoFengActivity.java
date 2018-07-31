3bdeeb1373bcfbf0bf99f1f9b270ae079c85e371
binhe729 <hb335623291@163.com>
2018-07-31 22:08:49 +0800

    First commit.

---
+++ app/src/main/java/com/example/administrator/zhaofengapplication/ZhaoFengActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.zhaofengapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ZhaoFengActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zhao_feng);
+    }
+}
